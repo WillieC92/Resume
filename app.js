3a09e277dc8453efe178a8330fa302a719771d3e
WillieC92 <williecolvin92@gmail.com>
2021-05-13 13:21:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+let myname = "Name: WILLIE COLVIN";
+
+{
+  console.log(myname);
+}
+
+let Career = "Career: Full Stack Developer / Student";
+
+{
+  console.log(Career);
+}
+
+let Description =
+  "Description: Learning Full Stack Development to enter into the workforce as a Full Stack Development professional.";
+
+{
+  console.log(Description);
+}
+
+// let Interest = "My Interest:*Gaming *Bodybuilding *Anime *Building *Computers";
+// {
+//   console.log(Interest);
+// }
+
+let i = "My Interest:"
+{console.log(i)}
+
+let g = "*Gaming"
+{console.log(g)}
+
+let b = "*Bodybuliding"
+{console.log(b)}
+
+let a = "*Anime"
+{console.log(a)}
+
+let t = "*Tech"
+{console.log(t)}
+
+
+let Experience = "My Previous Experience:"
+{console.log(Experience)}
+
+let Exp = "*Recruiter, Health Coach, Personal Trainer"
+{console.log(Exp)}
+
+let Skills = "*My Skills:"
+{console.log(Skills)}
+
+let s = "*Motivational Interviewing, Functional Movement, Diabetic Prevention, Nutrition Assessment, Exercise Rx, Meal Planning"
+{console.log(s)}
